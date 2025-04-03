653221c386a620faadb8575752b0b296da16d6d3
deveshcse <d.mishra56603@gmail.com>
2025-04-03 13:26:43 +0530

    add React Query and Devtools for data fetching and state management

---
+++ src/app/AppProvider.tsx
@@ -0,0 +1,17 @@
+'use client';
+import React from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Toaster } from "@/components/ui/sonner"
+
+const queryClient = new QueryClient();
+
+export const AppProvider = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <QueryClientProvider client={queryClient}>
+        {children}
+        <Toaster />
+        <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+};
