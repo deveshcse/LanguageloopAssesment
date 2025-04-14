e6b1651be6888ef8cf0c02dec08181ace1412484
deveshcse <d.mishra56603@gmail.com>
2025-04-14 11:54:52 +0530

    feat: add SheetExample component for enhanced UI interaction in PaginatedPostsPage

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,9 +1,11 @@
 "use client";
 import { ClientSidePagination } from "./ClientSidePagination";
 import { ServerSidePagination } from "./ServerSidePagination";
+import SheetExample from "./SheetExample";
 export default function PaginatedPostsPage() {
   return (
     <div className="m-10">
+      <SheetExample />
       <ServerSidePagination />
       <ClientSidePagination />
     </div>
--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,26 +1,10 @@
-"use client";
 
-import { Button } from "@/components/ui/button";
-import { defineAbilityFor } from "@/lib/defineAbilities";
-import { ACTIONS, PAGES } from "@/lib/permissions";
 
-const userType: "admin" | "user" = "user";
-const ability = defineAbilityFor(userType);
 
 export default function SettingsPage() {
-  const canView = ability.can(ACTIONS.VIEW, PAGES.SETTINGS);
-  const canDelete = ability.can(ACTIONS.DELETE, PAGES.SETTINGS);
-  const canCreate = ability.can(ACTIONS.CREATE, PAGES.SETTINGS);
-  const canUpdate = ability.can(ACTIONS.UPDATE, PAGES.SETTINGS);
-
-  if (!canView) return <div>Unauthorized</div>;
-
-  return (
+   return (
     <div className="p-6 space-y-4">
-      {canDelete && <Button className="btn">Delete</Button>}
-      {canCreate && <Button className="btn">+ Create</Button>}
-      {canUpdate && <Button className="btn">Edit</Button>}
-      <h1 className="text-2xl font-bold">Settings</h1>
+        <h1 className="text-2xl font-bold">Settings</h1>
     </div>
   );
 }
