8db20c7c4d0f08424db80cc557591f4c021824cb
deveshcse <d.mishra56603@gmail.com>
2025-04-15 16:04:21 +0530

    refactor: clean up formatting and remove unnecessary whitespace in ServerSidePagination component

--- src/app/posts/ServerSidePagination.tsx
+++ src/app/posts/ServerSidePagination.tsx
@@ -1,4 +1,3 @@
-
 "use client";
 
 import { useState } from "react";
@@ -8,39 +7,44 @@ import { columns } from "./columns";
 import { DataTable } from "@/components/DataTable";
 
 
-import React from 'react'
-
 export const ServerSidePagination = () => {
+  const [page, setPage] = useState(1);
+  const limit = 10;
 
-    const [page, setPage] = useState(1);
-    const limit = 10;
-  
-    const { data, isLoading, isError, isFetching,  isPlaceholderData } = usePosts(page, limit);
+  const { data, isLoading, isError, isFetching, isPlaceholderData } = usePosts(
+    page,
+    limit
+  );
 
-    console.log("ispreviousdata", isPlaceholderData);
+  console.log("ispreviousdata", isPlaceholderData);
 
-  
-    if (isLoading) return <div>Loading...</div>;
-    if (isError || !data) return <div>Error fetching posts</div>;
+  if (isLoading) return <div>Loading...</div>;
+  if (isError || !data) return <div>Error fetching posts</div>;
 
-  
-    return (
-        <div className="space-y-4">
-            <h1 className="text-2xl font-bold mb-4">Server Side pagination</h1>
-          <DataTable columns={columns} data={data.posts} />
-    
-          <div className="flex justify-between items-center">
-            <Button disabled={page === 1 || isFetching || isPlaceholderData} onClick={() => setPage(p => p - 1)}>
-              Previous
-            </Button>
-            <span>Page {page} of {Math.ceil((data.total)/limit)}</span>
-            <Button
-              disabled={(page * limit) >= data.total || isFetching || isPlaceholderData}
-              onClick={() => setPage(p => p + 1)}
-            >
-              {isFetching? "Loading..." : "Next"}
-            </Button>
-          </div>
-        </div>
-      );
-}
+  return (
+    <div className="space-y-4">
+      <h1 className="text-2xl font-bold mb-4">Server Side pagination</h1>
+      <DataTable columns={columns} data={data.posts} />
+
+      <div className="flex justify-between items-center">
+        <Button
+          disabled={page === 1 || isFetching || isPlaceholderData}
+          onClick={() => setPage((p) => p - 1)}
+        >
+          Previous
+        </Button>
+        <span>
+          Page {page} of {Math.ceil(data.total / limit)}
+        </span>
+        <Button
+          disabled={
+            page * limit >= data.total || isFetching || isPlaceholderData
+          }
+          onClick={() => setPage((p) => p + 1)}
+        >
+          {isFetching ? "Loading..." : "Next"}
+        </Button>
+      </div>
+    </div>
+  );
+};
